6d5bffcead1901379f368adaaf3f5688016067f3
Synzx <kenxflame@gmail.com>
2025-10-13 15:25:05 +0530

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,9 @@
+echo "0 30000" > /proc/shell-temp
+echo "1 30000" > /proc/shell-temp
+echo "2 30000" > /proc/shell-temp
+echo "3 30000" > /proc/shell-temp
+echo "4 30000" > /proc/shell-temp
+echo "5 30000" > /proc/shell-temp
+echo "6 30000" > /proc/shell-temp
+echo "7 30000" > /proc/shell-temp
+
